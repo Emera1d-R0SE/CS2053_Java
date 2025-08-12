1dda0ed8081d6eb9fc257388313d0eb811088cdb
Amrithaa M <emerald42rose@outlook.com>
2025-08-12 10:53:56 +0530

    Create program3.java

---
+++ Lab1/program3.java
@@ -0,0 +1,18 @@
+//reverse number (543=345)
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        int reverse=0;
+        while(num!=0) {
+            int digit = num%10;
+            reverse=reverse*10+digit;
+            num/=10;
+        }
+
+        System.out.println("reverse-> " + reverse);
+    }
+}
